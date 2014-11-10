bb9c4ac77e10cc8ef9518fae992667182a78ff78
Max Radermacher <max.radermacher@gmail.com>
2014-11-11 00:36:24 +0100

    Update examples to use new Simulation methods.
    In order to remain consistent with the changed methods in the Simulation class,
    the method calls in this class needed to be updated.  Its now significantly
    easier to add nodes to a network/simulation.

--- src/simizerexamples/QualifExample.java
+++ src/simizerexamples/QualifExample.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package simizerexamples;
 
 import java.util.logging.Level;
@@ -23,37 +19,37 @@ import simizer.utils.SimizerUtils;
  * @author isep
  */
 public class QualifExample {
-    public static void main(String ... args) {
-        Simulation sim = new Simulation(20000);
-         //LAN
-        Network lan = new Network(new UniLaw(0));
-        sim.addNetwork("net", lan);
-        StorageElement.setFactory(SimizerUtils.getDefaultResourceFactory());
-        Processor p = new NewProcessor(2, 1000.0);
-        ServerNode sn = new ServerNode(0, 512000, 50, 0.0, new StorageElement(5120000,2), p);
-        
-        LBNode  frontend = new LBNode(1, new RoundRobin());
-        sn.setFrontendNode(frontend);
-        sim.addNodeToNet(sn,"net");
-        sim.addNodeToNet(frontend,"net");
-        
-        int typeId = 0;
-        String params = "p1=1";
-        int arTime = 0;
-        String type = "read";
-        int nbInst = 10000;
-        Request r1 = new Request(0,typeId,arTime, params,50, type, nbInst * 1000000L);
-        Request r2 = new Request(1,typeId,arTime, params,50, type, nbInst * 1000000L);
-        Request r3 = new Request(2,typeId,arTime, params,50, type, nbInst * 1000000L);
-        
-              
-        lan.send(null, sn,r1 , 0);
-        lan.send(null, sn, r2, 0);
-        lan.send(null, sn, r3, 0);
-        try {
-            sim.runSim();
-        } catch (Exception ex) {
-            Logger.getLogger(QualifExample.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
+  public static void main(String... args) {
+    Simulation sim = new Simulation(20000);
+    //LAN
+    Network lan = new Network(new UniLaw(0));
+    sim.addNetwork("net", lan);
+    StorageElement.setFactory(SimizerUtils.getDefaultResourceFactory());
+    Processor p = new NewProcessor(2, 1000.0);
+    ServerNode sn = new ServerNode(0, 512000, 50, 0.0, new StorageElement(5120000, 2), p);
+
+    LBNode frontend = new LBNode(1, new RoundRobin());
+    sn.setFrontendNode(frontend);
+    sim.addNodeToNet(sn, "net");
+    sim.addNodeToNet(frontend, "net");
+
+    int typeId = 0;
+    String params = "p1=1";
+    int arTime = 0;
+    String type = "read";
+    int nbInst = 10000;
+    Request r1 = new Request(0, typeId, arTime, params, 50, type, nbInst * 1000000L);
+    Request r2 = new Request(1, typeId, arTime, params, 50, type, nbInst * 1000000L);
+    Request r3 = new Request(2, typeId, arTime, params, 50, type, nbInst * 1000000L);
+
+    lan.send(null, sn, r1, 0);
+    lan.send(null, sn, r2, 0);
+    lan.send(null, sn, r3, 0);
+    try {
+      sim.runSim();
+    } catch (Exception ex) {
+      Logger.getLogger(QualifExample.class.getName()).log(Level.SEVERE, null, ex);
     }
+  }
 }
--- src/simizerexamples/SimizerExamples.java
+++ src/simizerexamples/SimizerExamples.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package simizerexamples;
 
 import java.io.IOException;
@@ -25,65 +21,43 @@ import simizer.storage.StorageElement;
  */
 public class SimizerExamples {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        ClientNode.configureLaws(
-                new GaussianLaw(15), 
-                new GaussianLaw(15), 
-                new ExponentialLaw(10000,500.0));
-        try {
-            ClientNode.configureRequestFactory(
-                    new RequestFactory(RequestFactory.loadRequests("./reqs.csv")));
-        } catch (IOException ex) {
-            Logger.getLogger(SimizerExamples.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      
-       //1. Network creation
-        Network net = new Network(new GaussianLaw(15));
-        
-       //2. machines creation
-        VM vm1 =new VM(1, net);
-        vm1.deploy(new LoadBalancerApp(0, 20000, new RoundRobin()));
-//        VM vmApp0 = new VM(2,net),
-//            vmApp1 = new VM(3,net);
-        InterpolationApp cap1 = new InterpolationApp(0);
-        cap1.setFrontend(1);
-        InterpolationApp cap2 = new InterpolationApp(0);
-        cap2.setFrontend(1);
-        ResourceFactory rf = new ResourceFactory(1000, 2000, 1024);
-        StorageElement.setFactory(rf);
-//        StorageElement se = new StorageElement(5120000000L, 10L);
-//        se.write(rf.getStartList());
-//        vmApp0.setStorage(se);
-//        se = new StorageElement(5120000000L, 10L);
-//         se.write(rf.getStartList());
-//        vmApp1.setStorage(se);
-        
-        //3. Client creation
-        ClientNode cn1 = new ClientNode(4,net, 0);
-        ClientNode cn2 = new ClientNode(5, net, 0);
-        
-        cn1.setServiceAddress(vm1);
-        cn2.setServiceAddress(vm1);
-        Simulation sim = new Simulation(20000);
-        sim.addClient(cn2);
-        sim.addClient(cn1);
-        sim.addNetwork("net", net);
-        sim.addNodeToNet(vm1, "net");
-//        sim.addNodeToNet(vmApp1,"net");
-//        sim.addNodeToNet(vmApp0, "net");
-        sim.addNodeToNet(cn2, "net");
-        sim.addNodeToNet(cn1, "net");
-//        vmApp0.deploy(cap1);
-//        vmApp1.deploy(cap2);
-//        
-        try {
-            sim.runSim();
-        } catch (Exception ex) {
-            Logger.getLogger(SimizerExamples.class.getName()).log(Level.SEVERE, null, ex);
-        }
+  public static void main(String[] args) {
+
+    ClientNode.configureLaws(
+            new GaussianLaw(15),
+            new GaussianLaw(15),
+            new ExponentialLaw(10000, 500.0));
+    try {
+      ClientNode.configureRequestFactory(
+              new RequestFactory(RequestFactory.loadRequests("./reqs.csv")));
+    } catch (IOException ex) {
+      Logger.getLogger(SimizerExamples.class.getName()).log(Level.SEVERE, null, ex);
     }
+
+    //1. Network creation
+    Network net = new Network(new GaussianLaw(15));
+
+    //2. machines creation
+    VM vm1 = new VM(1, net);
+    vm1.deploy(new LoadBalancerApp(0, 20000, new RoundRobin()));
+
+    ResourceFactory rf = new ResourceFactory(1000, 2000, 1024);
+    StorageElement.setFactory(rf);
+
+    //3. Client creation
+    ClientNode cn1 = new ClientNode(4, net, 0);
+    ClientNode cn2 = new ClientNode(5, net, 0);
+
+    cn1.setServiceAddress(vm1);
+    cn2.setServiceAddress(vm1);
+
+    Simulation sim = new Simulation(20000);
+    sim.toNetworkAddNodes(net, vm1, cn1, cn2);
+
+    try {
+      sim.runSim();
+    } catch (Exception ex) {
+      Logger.getLogger(SimizerExamples.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
 }
