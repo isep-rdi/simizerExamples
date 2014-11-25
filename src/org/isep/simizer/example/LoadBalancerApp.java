c53ffb38921daa0a3f6ee62e75868a59ee34b834
Max Radermacher <max.radermacher@gmail.com>
2014-11-25 15:00:36 +0100

    Reformat the LoadBalanceApp class.

--- src/org/isep/simizer/example/LoadBalancerApp.java
+++ src/org/isep/simizer/example/LoadBalancerApp.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.isep.simizer.example;
 
 import java.util.HashMap;
@@ -12,98 +8,99 @@ import simizer.app.Application;
 import simizer.requests.Request;
 
 /**
- * Class for handling and applying load balancing policies.
- * Nodes register to the application through requests sending.
+ * Class for handling and applying load balancing policies. Nodes register to
+ * the application through requests sending.
+ *
  * @author Sylvain Lefebvre
- * 
  */
 public class LoadBalancerApp extends Application {
 
-    private final Policy pol;
-    private final Policy.Callback pac;
-    private final Map<Long, Node> pending = new HashMap<>();
+  private final Policy pol;
+  private final Policy.Callback pac;
+  private final Map<Long, Node> pending = new HashMap<>();
 
-    public LoadBalancerApp(int id, int memSize, Policy pol) {
-        super(id, memSize);
-        this.pol = pol;
-        if (pol instanceof Policy.Callback) {
-          this.pac = (Policy.Callback) pol;
-        } else {
-          this.pac = null;
-        }
+  public LoadBalancerApp(int id, int memSize, Policy pol) {
+    super(id, memSize);
+    this.pol = pol;
+    if (pol instanceof Policy.Callback) {
+      this.pac = (Policy.Callback) pol;
+    } else {
+      this.pac = null;
     }
-    /**
-     * Checks whether the request is an application request, a registration request or a response.
-     * forwards to the appropriate method.
-     * @param orig
-     * @param req 
-     */
+  }
 
-    @Override
-    public void handle(Node orig, Request req) {
+  /**
+   * Checks whether the request is an application request, a registration
+   * request or a response. forwards to the appropriate method.
+   *
+   * @param orig
+   * @param req
+   */
 
-        if (req.getFtime() == 0) { // Registration or application request
-            if (req.getParameters().equals("register")) {
-                handleRegisterRequest(orig, req);
-            } else {
-                handleAppRequest(orig, req);
-            }
-        } else {
-            handleResponse(orig, req);
-
-        }
+  @Override
+  public void handle(Node orig, Request req) {
+    if (req.getFtime() == 0) { // Registration or application request
+      if (req.getParameters().equals("register")) {
+        handleRegisterRequest(orig, req);
+      } else {
+        handleAppRequest(orig, req);
+      }
+    } else {
+      handleResponse(orig, req);
     }
-    /**
-     * Called if the request is an application request, applies the specified load balancing policy
-     * and records the client for sending the response.
-     * 
-     * @param orig
-     * @param req 
-     */
-    public void handleAppRequest(Node orig, Request req) {
-        pending.put(req.getId(), orig);
+  }
 
-        long start = System.nanoTime();
-        Node target = pol.loadBalance(req);
-        if(target==null) {
-            req.setError(1);
-            vm.send(req, pending.remove(req.getId()));
-        } else {
-            req.setFwdTime(System.nanoTime() - start);
-            vm.send(req,target);
-        }
+  /**
+   * Called if the request is an application request, applies the specified load
+   * balancing policy and records the client for sending the response.
+   *
+   * @param orig
+   * @param req
+   */
+  public void handleAppRequest(Node orig, Request req) {
+    pending.put(req.getId(), orig);
 
-    }
-    /**
-     * Called when a "register" request is received, 
-     * adds the originating node to the load balanced machines list in the policy.
-     * 
-     * @param orig
-     * @param req 
-     */
-    public void handleRegisterRequest(Node orig, Request req) {
-        pol.addNode(orig);
-    }
-    
-    /**
-     * Sends the response to the client that sent the request,
-     * removes the client from the pending list.
-     * @param orig
-     * @param req 
-     */
-
-    public void handleResponse(Node orig, Request req) {
-       
-        if (pac != null) {
-            pac.receivedRequest(orig, req);
-        }
-        if (pending.containsKey(req.getId())) {
-            vm.send(req, pending.remove(req.getId()));
-        }
-
+    long start = System.nanoTime();
+    Node target = pol.loadBalance(req);
+    if (target == null) {
+      req.setError(1);
+      vm.send(req, pending.remove(req.getId()));
+    } else {
+      req.setFwdTime(System.nanoTime() - start);
+      vm.send(req, target);
     }
 
-    @Override
-    public void init() {
+  }
+
+  /**
+   * Called when a "register" request is received, adds the originating node to
+   * the load balanced machines list in the policy.
+   *
+   * @param orig
+   * @param req
+   */
+  public void handleRegisterRequest(Node orig, Request req) {
+    pol.addNode(orig);
+  }
+
+  /**
+   * Sends the response to the client that sent the request, removes the client
+   * from the pending list.
+   *
+   * @param orig
+   * @param req
+   */
+  public void handleResponse(Node orig, Request req) {
+    if (pac != null) {
+      pac.receivedRequest(orig, req);
     }
+    if (pending.containsKey(req.getId())) {
+      vm.send(req, pending.remove(req.getId()));
+    }
+  }
+
+  @Override
+  public void init() {
+  }
+
 }
